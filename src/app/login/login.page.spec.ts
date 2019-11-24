6c8683a66d15fed7d4e16497512f818cb0b00679
finalseed <finalseed08@gmail.com>
2019-11-25 00:08:17 +0700

    Signed-off-by: finalseed <finalseed08@gmail.com>

---
+++ src/app/login/login.page.spec.ts
@@ -0,0 +1,32 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import {LoginPage } from './login.page';
+
+describe('LoginPage', () => {
+  let component: LoginPage;
+  let fixture: ComponentFixture<LoginPage>;
+  let loginPage: HTMLElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginPage ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoginPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have a list of 10 elements', () => {
+    loginPage = fixture.nativeElement;
+    const items = loginPage.querySelectorAll('ion-item');
+    expect(items.length).toEqual(10);
+  });
+
+});
